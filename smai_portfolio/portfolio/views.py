91cb591cfbc82a8b91f5c95866bbc9b7ce312582
sami-mai <samirah.maison@gmail>
2018-05-02 14:07:41 +0300

    add view funtion for home page

--- smai_portfolio/portfolio/views.py
+++ smai_portfolio/portfolio/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+# from django.http import HttpResponse, Http404
+from django.shortcuts import render, redirect
+# import datetime as dt
+# from .models import Article
+
 
 # Create your views here.
+def home(request):
+    return render(request, 'index.html')
