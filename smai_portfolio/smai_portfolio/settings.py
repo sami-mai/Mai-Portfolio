096064e4d2bcaa50a587eb25f73daff5dcdf5ded
sami-mai <samirah.maison@gmail>
2018-05-02 14:10:21 +0300

    register apps and database

--- smai_portfolio/smai_portfolio/settings.py
+++ smai_portfolio/smai_portfolio/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'portfolio',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,9 +77,11 @@ WSGI_APPLICATION = 'smai_portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mai_portfolio',
+        'USER': 'sami_mai',
+        'PASSWORD': 'SmaiDB',
+        }
 }
 
 
